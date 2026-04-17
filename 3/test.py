4d1552ef68d7a06fedb3e40bc7a4e4322d75b69c
Gadeti <gadeti23@gmail.com>
2026-04-16 21:11:46 -0500

    Implement test function for ANTLR parser
    Add a test function for parsing input strings using ANTLR.

---
+++ 3/test.py
@@ -0,0 +1,19 @@
+import sys
+from antlr4 import *
+from PruebaLL1Lexer import PruebaLL1Lexer
+from PruebaLL1Parser import PruebaLL1Parser
+
+def test(input_str):
+    input_stream = InputStream(input_str)
+    lexer = PruebaLL1Lexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = PruebaLL1Parser(stream)
+    
+    # Intentar construir el árbol de derivación
+    tree = parser.program()
+    print(f"Entrada: '{input_str}' -> Procesada correctamente")
+
+if __name__ == '__main__':
+    # Pruebas basadas en la tabla de análisis
+    test("ab") # Deriva de ReglaS1: A(eps) a A(eps) b
+    test("ba") # Deriva de ReglaS2: B(eps) b B(eps) a
